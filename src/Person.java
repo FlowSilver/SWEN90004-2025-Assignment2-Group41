935dd662eed7bbb7d727c45453525ded0ca4b02c
Joshua Lapitan <josh.lapitan@icloud.com>
2025-05-25 06:14:05 +1000

    Completed setup

--- src/Person.java
+++ src/Person.java
@@ -1,13 +1,24 @@
+/* This class is in charge of how people move within the system */
+
 package src;
+import java.util.Random;
 
+public class Person extends Thread {
+    private int lifeExpectancy;
+    private int age; 
+    private int metabolism;
+    private int wealth;
+    private int vision;
 
-public class Person {
-    private int lifeNum;
+    // Creation of a person will generate values to make them have unique attributes
+    // Welath isn't inherited in this system.
     public Person() {
-        this.lifeNum = Params.lifeExpectancy();
+        this.lifeExpectancy = Params.lifeExpectancy();
+        Random random = new Random();
+        this.age = random.nextInt(this.lifeExpectancy); // even if a person is created, their actual age is still randomise
+        this.metabolism = Params.metabolism();
+        this.wealth = random.nextInt(50) + this.metabolism;
+        this.vision = Params.vision();
     }
-
-    public int lifeTotal() {
-        return lifeNum;
-    }
+    
 }
--- src/World.java
+++ src/World.java
@@ -1,12 +1,89 @@
+/* This is the class that simulates the model.
+ * It mainly controls the patches and their coordinates.
+ * People are also created here.
+ */
+
 package src;
-import java.io.*;
-import java.util.*;
 
 public class World {
-    public static void main (String[] args) {
-        for (int i = 0; i<Params.TOTAL_PEOPLE+1; i++) {
-            Person person = new Person();
-            System.out.println(person.lifeTotal());
+    public static final int maxCoord = 51; //not a parameter; determines max size of map
+    private Patch[][] map; 
+
+    // This is synonymous to the setup phase in the NetLogo version of the program.
+    public World() {
+        setupPatches();
+        setupPeople();
+    }
+
+    //--- Setup functions ---//
+
+    // DELETE WHEN CONSIDERED: you may want to add a people list if you want easy access to them, for example if you do this non concurrently
+
+    // Creates all the people and places them randomly on the map
+    public void setupPeople() {
+        for (int i=0; i<Params.NUM_PEOPLE; i++) {
+            Patch patch = map[Params.rollCoord()][Params.rollCoord()];
+            if (patch.getPerson() == null) {
+                patch.setPerson(new Person());
+            }
         }
     }
+
+    // Creates and populates the world with patches of various levels of productivity
+    public void setupPatches() {
+
+        // Initial creation of patches
+        this.map = new Patch[maxCoord][maxCoord];
+        for (int x = 0; x<maxCoord; x++) {
+            for (int y = 0; y<maxCoord; y++) {
+                map[x][y] = new Patch(x, y);
+            }
+        }
+
+        // Spreads the grain around the map tops up the best grain patches
+        for (int k=0; k<5; k++) {
+            for (int i = 0; i<maxCoord; i++) {
+                for (int j = 0; j<maxCoord; j++) {
+                    if (map[i][j].getMaxGrain() != 0) {
+                        map[i][j].setGrain(map[i][j].getGrain());
+                    }
+                }
+            }
+            diffusePatches();
+        }
+
+        // Continue to spread grain around without topping up the original supply
+        for (int k=0; k<10; k++) {
+            diffusePatches();
+        }
+
+        // Sets the current grain amount as the maximum for each patch
+        for (int i = 0; i<maxCoord; i++) {
+            for (int j = 0; j<maxCoord; j++) {
+                map[i][j].floorGrain();
+                map[i][j].setMaxGrain();
+            }
+        }  
+    }
+    
+    // Causes diffusal in all grains in the map
+    public void diffusePatches() {
+        for (int i = 0; i<maxCoord; i++) {
+            for (int j = 0; j<maxCoord; j++) {
+                map[i][j].diffuse(map);
+            }
+        }
+    }
+
+
+    //--- Getter and setter functions ---//
+
+    public Patch[][] getMap() {
+        return map;
+    }
+
+    public void setMap(Patch[][] map) {
+        this.map = map;
+    }
+
 }
