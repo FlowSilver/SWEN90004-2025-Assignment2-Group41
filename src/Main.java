721036f90119b9bf9c8f5e12e9e84d80f9ee2c15
FlowSilver <175792972+FlowSilver@users.noreply.github.com>
2025-05-30 13:21:11 +1000

    Add simulation-only option

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 /* This is where the model will run */
 
+import person.strategy.DefaultStrategy;
+import util.Params;
+import world.Patch;
 import world.Simulation;
+import world.World;
 
 /**
  * Main class to run the simulation model.
@@ -17,10 +21,15 @@ public class Main {
      * and reproduction strategies.
      */
     public static void main(String[] args) {
+        // Comment out if only wish to see simulation results
         Simulation simulation = new Simulation();
         simulation.simulateVariousGrowthRate();
         simulation.simulateVariousPopulation();
         simulation.simulateVariousReproductionRule();
+
+        // Only Simulation, do not write results to csv files
+//        World world = new World(Params.NUM_PEOPLE, Params.GRAIN_GROWTH_INTERVAL, new DefaultStrategy());
+//        world.runSimulation();
     }
 
 }
